30d592fdbbe5ed03ff24b00e6e0e25d58887e8fd
Erlind Sejdiu <erlindamprogrammieren@gmail.com>
2025-06-12 02:23:04 +0200

    Working on drag and drop

--- Elevate/Views/CombineTaskPage.xaml.cs
+++ Elevate/Views/CombineTaskPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Maui.Controls; // Make sure this is included for ContentPage
 using Elevate.ViewModels;
+using Elevate.Models;
 namespace Elevate
 {
     public partial class CombineTaskPage : ContentPage
@@ -9,5 +10,37 @@ namespace Elevate
             InitializeComponent();
             BindingContext = viewModel; // This correctly sets the BindingContext via DI
         }
+
+        void OnDragStarting(object sender, DragStartingEventArgs e)
+        {
+            if (sender is BindableObject bindable &&
+                bindable.BindingContext is GroupElevateTask draggedTask)
+            {
+                e.Data.Properties["DraggedGroup"] = draggedTask;
+            }
+        }
+
+        void OnDrop(object sender, DropEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("OnDrop triggered");
+
+            if (e.Data.Properties.TryGetValue("DraggedGroup", out var draggedObj) &&
+                draggedObj is GroupElevateTask draggedGroup)
+            {
+                if (sender is BindableObject bindable &&
+                    bindable.BindingContext is GroupElevateTask targetGroup &&
+                    !ReferenceEquals(draggedGroup, targetGroup))
+                {
+                    targetGroup.Add(draggedGroup);
+                    System.Diagnostics.Debug.WriteLine($"Added {draggedGroup.Name} to {targetGroup.Name}");
+
+                    if (BindingContext is CombineTaskViewModel vm)
+                    {
+                        vm.Projects.Remove(draggedGroup);
+                    }
+                }
+            }
+        }
+
     }
 }
