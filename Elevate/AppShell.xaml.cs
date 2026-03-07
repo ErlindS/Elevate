d8519f5f4f4009afe2c4ec3f86c7cb1a06ef1c68
Erlind Sejdiu <erlindamprogrammieren@gmail.com>
2026-03-07 22:08:23 +0100

    more style changes

--- Elevate/AppShell.xaml.cs
+++ Elevate/AppShell.xaml.cs
@@ -6,5 +6,23 @@
         {
             InitializeComponent();
         }
+
+        private void OnThemeToggleClicked(object sender, EventArgs e)
+        {
+            if (Application.Current == null) return;
+
+            if (Application.Current.UserAppTheme == AppTheme.Dark)
+            {
+                Application.Current.UserAppTheme = AppTheme.Light;
+                if (sender is Button btn)
+                    btn.Text = "🌙 Dark Mode";
+            }
+            else
+            {
+                Application.Current.UserAppTheme = AppTheme.Dark;
+                if (sender is Button btn)
+                    btn.Text = "☀️ Light Mode";
+            }
+        }
     }
 }
