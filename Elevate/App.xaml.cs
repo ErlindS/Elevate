de4511e42a0f2979256d9cf928013f39cfb6edb3
Erlind Sejdiu <erlindamprogrammieren@gmail.com>
2026-03-07 22:26:18 +0100

    Design update

--- Elevate/App.xaml.cs
+++ Elevate/App.xaml.cs
@@ -30,6 +30,38 @@ namespace Elevate
         protected override Window CreateWindow(IActivationState activationState)
         {
             var window = base.CreateWindow(activationState);
+
+            window.TitleBar = new TitleBar
+            {
+                Title = "Elevate",
+                BackgroundColor = Colors.White,
+                ForegroundColor = Color.FromArgb("#3B7A57"),
+                HeightRequest = 40
+            };
+
+#if WINDOWS
+            window.HandlerChanged += (s, e) =>
+            {
+                var mauiWindow = s as Window;
+                if (mauiWindow?.Handler?.PlatformView is Microsoft.UI.Xaml.Window winUIWindow)
+                {
+                    var titleBar = winUIWindow.AppWindow.TitleBar;
+                    if (titleBar != null)
+                    {
+                        var primaryDark = global::Windows.UI.Color.FromArgb(255, 59, 122, 87);
+                        var hoverBg = global::Windows.UI.Color.FromArgb(255, 235, 240, 235);
+
+                        titleBar.ButtonForegroundColor = primaryDark;
+                        titleBar.ButtonHoverBackgroundColor = hoverBg;
+                        titleBar.ButtonHoverForegroundColor = primaryDark;
+                        titleBar.ButtonPressedBackgroundColor = hoverBg;
+                        titleBar.ButtonPressedForegroundColor = primaryDark;
+                        titleBar.ButtonInactiveForegroundColor = global::Windows.UI.Color.FromArgb(255, 91, 168, 130);
+                    }
+                }
+            };
+#endif
+
             return window;
         }
     }
